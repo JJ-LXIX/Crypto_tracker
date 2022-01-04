84ecd60ac586319bb0147945d7d7428725b2476c
Jagrath <jagrathshettylxix@gmail.com>
2022-01-04 11:56:35 +0530

    Header Component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Homepage from "./Pages/Homepage";
 import CoinPage from "./Pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
 
 function App() {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-      <div>
+      <div className={classes.App}>
         <Header />
         <Routes>
           <Route path="/" element={<Homepage />} exact />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,65 @@
+import {
+  AppBar,
+  Container,
+  Select,
+  Toolbar,
+  Typography,
+  MenuItem,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Rubik",
+    cursor: "pointer",
+  },
+}));
 
 const Header = () => {
-  return <div>Header</div>;
+  const classes = useStyles();
+
+  const navigate = useNavigate();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => navigate("/")}
+              className={classes.title}
+              variant="h6"
+            >
+              Crypto Tracker
+            </Typography>
+
+            <Select
+              variant="outlined"
+              style={{ width: 100, height: 40, marginRight: 15 }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
 };
 
 export default Header;
