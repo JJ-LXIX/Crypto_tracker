af398cfecab08bd5bf38ddfd7b52931581712326
Jagrath <jagrathshettylxix@gmail.com>
2022-03-17 12:31:06 +0530

    corrected spelling error

--- src/Components/Authentication/UserSidebar.jsx
+++ src/Components/Authentication/UserSidebar.jsx
@@ -74,7 +74,7 @@ export default function UserSidebar() {
     setAlert({
       open: true,
       type: "success",
-      message: "Logout Successfull !",
+      message: "Logout Successful !",
     });
 
     toggleDrawer();
