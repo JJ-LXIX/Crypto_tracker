6a27bd994c80205a9870e905bbb89f069e24bc98
Jagrath <jagrathshettylxix@gmail.com>
2022-03-18 12:13:46 +0530

    watchlist add and remove functionality added

--- src/Components/Authentication/UserSidebar.jsx
+++ src/Components/Authentication/UserSidebar.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { CryptoState } from "../../CryptoContext";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-
 import { Avatar, Button } from "@material-ui/core";
 import { signOut } from "firebase/auth";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
+import { numberWithCommas } from "../Banner/Carousel";
+import { AiFillDelete } from "react-icons/ai";
+import { doc, setDoc } from "firebase/firestore";
 
 const useStyles = makeStyles({
   container: {
@@ -48,6 +50,17 @@ const useStyles = makeStyles({
     gap: 12,
     overflowY: "scroll",
   },
+  coin: {
+    padding: 10,
+    borderRadius: 5,
+    color: "black",
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#eebc1d",
+    boxShadow: "0 0 3px black",
+  },
 });
 
 export default function UserSidebar() {
@@ -55,7 +68,7 @@ export default function UserSidebar() {
   const [state, setState] = React.useState({
     right: false,
   });
-  const { user, setAlert } = CryptoState();
+  const { user, setAlert, watchlist, coins, symbol } = CryptoState();
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
@@ -66,6 +79,31 @@ export default function UserSidebar() {
     }
 
     setState({ ...state, [anchor]: open });
+  };
+
+  const removeFromWatchList = async (coin) => {
+    const coinRef = doc(db, "watchlist", user.uid);
+
+    try {
+      await setDoc(
+        coinRef,
+        {
+          coins: watchlist.filter((watch) => watch !== coin?.id),
+        },
+        { merge: "true" }
+      );
+      setAlert({
+        open: true,
+        message: `${coin.name} Removed from the  Watchlist!`,
+        type: "success",
+      });
+    } catch (error) {
+      setAlert({
+        open: true,
+        message: error.message,
+        type: "error",
+      });
+    }
   };
 
   const logOut = () => {
@@ -124,6 +162,23 @@ export default function UserSidebar() {
                   <span style={{ fontSize: 15, textShadow: "0 0 5px black" }}>
                     Watchlist
                   </span>
+                  {/* eslint-disable-next-line */}
+                  {coins.map((coin) => {
+                    if (watchlist.includes(coin.id))
+                      return (
+                        <div className={classes.coin}>
+                          <span>{coin.name}</span>
+                          <span style={{ display: "flex", gap: 8 }}>
+                            {symbol}
+                            {numberWithCommas(coin.current_price.toFixed(2))}
+                            <AiFillDelete
+                              style={{ cursor: "pointer", fontSize: "16" }}
+                              onClick={() => removeFromWatchList(coin)}
+                            />
+                          </span>
+                        </div>
+                      );
+                  })}
                 </div>
               </div>
               <Button
