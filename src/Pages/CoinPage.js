7d9fcf79f64f06a334684950fb9905cd2d713349
Jagrath <jagrathshettylxix@gmail.com>
2022-03-18 11:53:43 +0530

    onSnapshot firebase functionality added

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -12,11 +12,13 @@ import {
 } from "@material-ui/core";
 import ReactHtmlParser from "react-html-parser";
 import { numberWithCommas } from "../Components/Banner/Carousel";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const CoinPage = () => {
   const { id } = useParams();
   const [coin, setCoin] = useState();
-  const { currency, symbol, user } = CryptoState();
+  const { currency, symbol, user, watchlist, setAlert } = CryptoState();
 
   const fetchCoin = async () => {
     const { data } = await axios.get(SingleCoin(id));
@@ -81,6 +83,54 @@ const CoinPage = () => {
       },
     },
   }));
+
+  const inWatchList = watchlist.includes(coin?.id);
+
+  const addToWatchList = async () => {
+    const coinRef = doc(db, "watchlist", user.uid);
+
+    try {
+      await setDoc(coinRef, {
+        coins: watchlist ? [...watchlist, coin?.id] : [coin?.id],
+      });
+      setAlert({
+        open: true,
+        message: `${coin.name} Added to the Watchlist!`,
+        type: "success",
+      });
+    } catch (error) {
+      setAlert({
+        open: true,
+        message: error.message,
+        type: "error",
+      });
+    }
+  };
+
+  const removeFromWatchList = async () => {
+    const coinRef = doc(db, "watchlist", user.uid);
+
+    try {
+      await setDoc(
+        coinRef,
+        {
+          coins: watchlist.filter((watch) => watch !== coin?.id),
+        },
+        { merge: "true" }
+      );
+      setAlert({
+        open: true,
+        message: `${coin.name} Removed from the  Watchlist!`,
+        type: "success",
+      });
+    } catch (error) {
+      setAlert({
+        open: true,
+        message: error.message,
+        type: "error",
+      });
+    }
+  };
 
   const classes = useStyles();
 
@@ -149,9 +199,14 @@ const CoinPage = () => {
           {user && (
             <Button
               variant="outlined"
-              style={{ width: "100%", height: 40, backgroundColor: "#eebc1d" }}
+              style={{
+                width: "100%",
+                height: 40,
+                backgroundColor: inWatchList ? "#ff0000" : "#eebc1d",
+              }}
+              onClick={inWatchList ? removeFromWatchList : addToWatchList}
             >
-              Add to Watchlist
+              {inWatchList ? "Remove from Watchlist" : "Add to Watchlist"}
             </Button>
           )}
         </div>
